3598fca0b46f17da038e626574a1d647611391c1
matildepetrilli <matilde.petrilli10@gmail.com>
2025-01-20 16:34:12 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+//lo colleghiamo dai baldings, cioè i componenti di react-redux.
+//questo componente andrà a racchiudere app, inserendo cosi lo store di redax
 
-createRoot(document.getElementById('root')).render(<App />)
+
+createRoot(document.getElementById("root")).render(
+  <Provider>
+    <App />
+  </Provider>
+);
